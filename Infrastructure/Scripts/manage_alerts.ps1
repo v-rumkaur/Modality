698f170672045fbf3c107f4a2dcfec4290c64de6
Sid Khanna <khannas@microsoft.com>
2025-01-30 22:13:58 +0000

    Merged PR 1281197: Getting powershellgallery from CRM.ICon.OneChat Artifact
    Getting package from config
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature
    
    #### PR Summary
    This pull request updates the deployment scripts to use a private PowerShell repository from CRM.ICon.OneChat for module installations.
    - Updated `deploy_azure_region_resources.ps1`, `manage_alerts.ps1`, and `deploy_azure_resources.ps1` to register and use the private repository for installing Azure modules.
    - Modified `Modality-Infrastructure-Pipeline.yml` to include steps for registering the private repository and installing modules from it.
    
    Related work items: #4519687

--- Infrastructure/Scripts/manage_alerts.ps1
+++ Infrastructure/Scripts/manage_alerts.ps1
@@ -16,8 +16,30 @@ param(
 $TemplateName = $ResourcePrefix + "-deployment"
 
 Write-Host "Setting up prerequisites"
-Install-Module AzureAD -Force
-Install-Module Az.Resources -Force
+Install-PackageProvider -Name NuGet -Force -Confirm:$false
+Write-Host "NuGet Package Provider Installed Successfully."
+
+$AccessToken = "$(System.AccessToken)"
+$SecureAccessToken = ConvertTo-SecureString $AccessToken -AsPlainText -Force
+$CredentialObj = New-Object System.Management.Automation.PSCredential("AzureDevOps", $SecureAccessToken)
+Write-Host "Credential object created."
+
+$CentralFeedName = "ModalityCentralFeed"
+$CentralFeedLocation = "https://pkgs.dev.azure.com/dynamicscrm/OneCRM/_packaging/CRM.ICon.OneChat/nuget/v2"
+        
+Write-Host "Started registering private central feed."
+if (-not (Get-PSRepository -Name $CentralFeedName -ErrorAction SilentlyContinue)) {
+    Register-PSRepository -Name $CentralFeedName -SourceLocation $CentralFeedLocation -InstallationPolicy Trusted -Credential $CredentialObj
+Write-Output "Repository '$CentralFeedName' registered successfully."
+} else {
+    Write-Output "Repository '$CentralFeedName' is already registered."
+}
+
+
+Get-InstalledModule -Name AzureRM -AllVersions | Uninstall-Module -Force -ErrorAction SilentlyContinue
+Write-Host "Installing Az modules."
+Install-Module AzureAD -Force -Repository $CentralFeedName -Credential $CredentialObj
+Install-Module Az.Resources -Force -Repository $CentralFeedName -Credential $CredentialObj
 Set-AzContext -SubscriptionName $SubscriptionName
                    
 Write-Host "Creating modality service alerts"
