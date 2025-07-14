2bda1780c5aad9e45b04641b71bd621917fa779c
Diane Tran <dianetran@microsoft.com>
2025-07-14 13:51:24 -0700

    revert unnecessary unit test udpate

--- CRM.ICon.Modality.Tests/Controllers/ModalityControllerTests.cs
+++ CRM.ICon.Modality.Tests/Controllers/ModalityControllerTests.cs
@@ -2,7 +2,6 @@ using CRM.ICon.Modality.Controllers;
 using CRM.ICon.Modality.Helpers.Cosmos;
 using CRM.ICon.Modality.Helpers.Telemetry;
 using CRM.ICon.Modality.Model;
-using CRM.ICon.Modality.Services.LiveChatSettings;
 using CRM.ICon.Modality.Services.Omnichannel;
 using CRM.ICon.Modality.Services.VDM;
 using Microsoft.AspNetCore.Authorization;
@@ -21,9 +20,8 @@ namespace CRM.ICon.Modality.Tests.Controllers
             var omnichannelService = Substitute.For<IOmnichannelService>();
             var telemetryService = Substitute.For<ITelemetryService>();
             var omniChannelEUService = Substitute.For<IOmnichannelEUService>();
-            var liveChatSettingsService = Substitute.For<ILiveChatSettingsService>();
             var cosmosDbClient = Substitute.For<ICosmosDbClient>();
-            return new ModalityController(vdmService, omnichannelService, liveChatSettingsService, telemetryService, omniChannelEUService, cosmosDbClient);
+            return new ModalityController(vdmService, omnichannelService, telemetryService, omniChannelEUService, cosmosDbClient);
         }
 
         [Theory]
