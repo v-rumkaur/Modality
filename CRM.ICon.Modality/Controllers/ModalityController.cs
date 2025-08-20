7265909c437f36bc8276b1e8003d27218fdb0524
Diane Tran <dianetran@microsoft.com>
2025-08-20 01:28:45 -0700

    pcs modality

--- CRM.ICon.Modality/Controllers/ModalityController.cs
+++ CRM.ICon.Modality/Controllers/ModalityController.cs
@@ -81,6 +81,12 @@ namespace CRM.ICon.Modality.Controllers
             modalityInfoPhone.IsAgentAvailable = true;
             modalityInfoPhone.InHoops = true;
 
+            ModalityInfo modalityInfoPCS = new ModalityInfo();
+            modalityInfoPhone.Modality = 5;
+            modalityInfoPhone.WaitTime = "0";
+            modalityInfoPhone.IsAgentAvailable = true;
+            modalityInfoPhone.InHoops = true;
+
             ICollection<LanguageSkillData> languageSkills = UserLcidToSkillData(userLCID);
 
             LanguageSkillData languageSkillData = languageSkills.FirstOrDefault();
@@ -213,6 +219,8 @@ namespace CRM.ICon.Modality.Controllers
                     modalityResponse.Modalities.Add(modalityInfoChat);
                 }
 
+                // hardcode PCS modality until real data is available in September
+                modalityResponse.Modalities.Add(modalityInfoPCS);
                 return Ok(modalityResponse);
             }
 
