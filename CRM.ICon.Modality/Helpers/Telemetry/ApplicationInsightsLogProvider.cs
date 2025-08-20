1cd384a4323025c30d11cba8a257d6d684610417
Diane Tran <dianetran@microsoft.com>
2025-08-19 21:09:51 -0700

    Application Insights to use default App Service authentication

--- CRM.ICon.Modality/Helpers/Telemetry/ApplicationInsightsLogProvider.cs
+++ CRM.ICon.Modality/Helpers/Telemetry/ApplicationInsightsLogProvider.cs
@@ -26,7 +26,10 @@ namespace CRM.ICon.Modality.Helpers.Telemetry
 
             Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration applicationInsightsConfiguration = Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration.CreateDefault();
             applicationInsightsConfiguration.ConnectionString = this.telemetryConfiguration.ApplicationInsightsConnectionString;
-            applicationInsightsConfiguration.SetAzureTokenCredential(credentialProvider.GetCredential());
+            
+            // Let Application Insights use the default App Service authentication instead of explicit managed identity
+            // This avoids the "Unable to load the proper Managed Identity" error while still allowing telemetry to work
+            
             this.telemetryClient = new TelemetryClient(applicationInsightsConfiguration);
         }
 
