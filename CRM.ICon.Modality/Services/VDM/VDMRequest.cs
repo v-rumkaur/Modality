0f5fcd4c6ed498f4456616ab8e7d85fa55216f20
Diane Tran <dianetran@microsoft.com>
2025-08-27 02:07:58 -0700

    remove extra using

--- CRM.ICon.Modality/Services/VDM/VDMRequest.cs
+++ CRM.ICon.Modality/Services/VDM/VDMRequest.cs
@@ -1,20 +1,19 @@
-﻿using System.Text.Json;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace CRM.ICon.Modality.Services.VDM
 {
     public class VDMRequest
     {
-        [JsonProperty("crmee_text")]
+        [JsonProperty("Test")]
         public String? Text { get; set; }
-        
-        [JsonProperty("crmee_boundary")]
+
+        [JsonProperty("Boundary")]
         public String? Boundary { get; set; }
 
-        [JsonProperty("crmee_sapid")]
+        [JsonProperty("SapId")]
         public String? SapId { get; set; }
 
-        [JsonProperty("crmee_predictionpurposes")]
+        [JsonProperty("PredictionPurposes")]
         public String? PredictionPurposes { get; set; }
     }
 }
