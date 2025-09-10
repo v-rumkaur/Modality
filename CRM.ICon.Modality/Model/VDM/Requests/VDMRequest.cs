eabc49a5b4a3288ad824da1b2f1453292c7da593
Diane Tran <dianetran@microsoft.com>
2025-09-10 12:20:23 -0700

    comments

--- CRM.ICon.Modality/Model/VDM/Requests/VDMRequest.cs
+++ CRM.ICon.Modality/Model/VDM/Requests/VDMRequest.cs
@@ -1,9 +1,8 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="MatchRuleRequest.cs" company="Microsoft Corporation">
+// <copyright file="VDMRequest.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace CRM.ICon.Modality.Model.VDM.Requests
--- CRM.ICon.Modality/Model/VDM/Responses/VDMResult.cs
+++ CRM.ICon.Modality/Model/VDM/Responses/VDMResult.cs
@@ -1,4 +1,9 @@
-﻿using System.Diagnostics.Contracts;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VDMResult.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System.Diagnostics.Contracts;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
